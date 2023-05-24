9ae151973281da5407762edeff60a0f3c502eb96
Adielson Medeiros <medeirosadielson@gmail.com>
2023-05-24 12:04:47 -0300

    adicionado button

--- App.js
+++ App.js
@@ -1,23 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet,TextInput, Text, View } from 'react-native';
+import { StyleSheet,TextInput, Text, View, Button } from 'react-native';
 
 function App() {
 
   const [nome,setNome]= useState('')
+  const [input, setInput] = useState('')
 
-  function pegaNome(texto){
+  function entrar(){
 
-    if (texto.length > 0 ){
-    setNome('Bem vindo '+ texto);
-    }else{
-      setNome('')
+    if(input === ''){
+      alert('Por favor, digite seu nome.');
+      return;
     }
+    setNome('Bem vindo '+ input);
   }
+
 
   return (
     <View>
-      <TextInput style={styles.input} placeholder="Digite seu nome" onChangeText={(text)=> pegaNome(text)}/>
+      <TextInput style={styles.input} placeholder="Digite seu nome"
+      onChangeText={(text)=> setInput(text)}/>
+      <Button title="Entrar" onPress={entrar}/>
       <Text style={styles.texto}>{nome}</Text>
     </View>
   )
@@ -33,10 +37,12 @@ const styles = StyleSheet.create({
     margin: 10,
     padding: 10,
     fontSize: 20,
+    marginTop:40
   },
   texto:{
     textAlign:'center',
-    fontSize: 25
+    fontSize: 25,
+    marginTop: 15
   }
 });
 export default App;
